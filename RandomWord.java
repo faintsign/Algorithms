51d5adcba6c6740138b5d8e58950b2f8ee1cef10
Faint <73570576+faintsign@users.noreply.github.com>
2022-04-13 10:18:29 -0400

    Add files via upload

---
+++ RandomWord.java
@@ -0,0 +1,33 @@
+/* *****************************************************************************
+ *  Name:              Faint
+ *  File: RandomWord.java
+ *  Project: Algorithms part 1
+ *  Coursera User ID:  123456
+ *  Last modified:     4/13/22
+ *  Description:
+ *  A program that selects a random word from standard input.
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        String champion = StdIn.readString();
+
+        while (!StdIn.isEmpty()) {
+            String candidate = StdIn.readString();
+            int i = 2;
+
+            if (StdRandom.bernoulli((double) 1 / i)) {
+                champion = candidate;
+            }
+
+            i++;
+        }
+
+        System.out.println("The random word is: " + champion);
+
+
+    }
+}
